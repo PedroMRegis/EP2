afa6afd12fcb483dfd8ac33e17a5edd99e032790
PedroMRegis <168037406+PedroMRegis@users.noreply.github.com>
2024-05-02 15:33:05 -0300

    Update Ep2.py
    Inicia o jogo e aloca os navios do jogador de acordo com as coordenadas escolhidas e aloca os navios do computador aleatoriamente. Após isso o jodo de verdade inicia e os navios ja podem atirar. O proximo código a ser colocado sera a parte final do jogo, passando por todos os tiros e definindo um vencedor.

--- Ep2.py
+++ Ep2.py
@@ -196,3 +196,142 @@ def substitui_mapa(mapa_computador, linha, coluna, tiro):
         mapa_computador[linha][coluna] = 'X'
     if tiro == "Água":
         mapa_computador[linha][coluna] = 'A'
+#--------------------------------------------------------------
+
+#Inicia-se o jogo
+if __name__ == '__main__':
+
+    print(
+    """
+ ===================================== 
+|                                     |
+| Bem-vindo ao INSPER - Batalha Naval |
+|                                     |
+ =======   xxxxxxxxxxxxxxxxx   ======= 
+
+Iniciando o Jogo!
+    """
+    )
+    # Imprime os paises e suas frotas frotas
+    for i, pais in enumerate(PAISES.items()):
+        print(f"{i+1}: {pais[0]}")
+        for navio, qtd in pais[1].items():
+            print(f"   {qtd} {navio}")
+    
+    # Armazenando escolha do jogador com tratamento de erro
+    opcoes = ('1','2','3','4','5')
+    while True:
+        jogador = str(input("\nQual o número da nação da sua frota? "))
+        if jogador in opcoes:
+            jogador = int(jogador)
+            break
+        else:
+            print(f"'{jogador}' é uma opção inválida! Tente novamente")
+
+    # Cria uma variável para armazenar a nação escolhida pelo jogador
+    escolhido = list(PAISES.keys())[jogador-1]
+    # Cria-se uma variável para o país que o computador irá representar
+    computador = random.choice(list(PAISES.keys()))
+
+    #printa qual nação foi escolhida pelo jogador e continua-se o jogo
+    print(f"\nVocê escolheu a nação: {escolhido}")
+    print(f"O computador escolheu a nação: {computador}")
+
+    # Cria os mapas utilizados no jogo, o do computador, o do computador visível para o jogador, e o do jogador
+    mapa_c = cria_mapa(10)
+    mapa_c_real = cria_mapa(10)
+    mapa_j = cria_mapa(10)
+
+    # Define as jogadas
+    jogadas_jogador = define_jogadas(escolhido)
+    jogadas_computador = define_jogadas(computador)
+
+    print(f"O computador está alocando os navios de guerra...")
+    # Estrutura para alocação da fropa do computador
+    for jogada in jogadas_computador:    
+        # Criando lista das possíveis jogadas do computador
+        posicoes_possiveis = []
+        for i in range(len(COLUNAS)):
+            for j in range(len(LINHAS)):
+                posicoes_possiveis.append(f"{COLUNAS[i]}{LINHAS[j]}")
+
+        while True:
+            pos = random.choice(posicoes_possiveis)
+            posicoes_possiveis.remove(pos)
+            blocos = CONFIGURACAO[jogada]
+            ori    = random.choice(('V', 'H'))
+            try:
+                mapa_c_real = aloca_navio(mapa_c_real, pos, blocos, ori, 'C')
+                break
+            except:
+                pass
+    print("O computador já está em posição de batalha!\n")
+        
+    # Exibe o tabuleiro
+    print_mapa(mapa_c, mapa_j)
+
+    # Estrutura para alocação da tropa do jogador
+    for jogada in range(len(jogadas_jogador)):
+        navio = jogadas_jogador[jogada]
+        blocos = CONFIGURACAO[navio]
+
+        # Imprime a jogada atual
+        if blocos > 1:
+            print(f"Alocar: {navio} ({blocos} blocos)")
+        else:
+            print(f"Alocar: {navio} (1 bloco)")
+        # Imprime as próximas jogadas
+        print(f"Próximos: { ', '.join(jogadas_jogador[jogada+1:]) }")
+
+        while True:
+            # Guarda a letra
+            while True:
+                opcoes = COLUNAS
+                letra = str(input("Informe a letra: ")).upper()
+                if letra in opcoes:
+                    break
+                else:
+                    print(f"'{letra}' é uma opção inválida! Tente novamente")
+            # Guarda a linha
+            while True:
+                opcoes = LINHAS
+                linha = input("Informe a linha: ")
+                if linha in opcoes:
+                    break
+                else:
+                    print(f"'{linha}' é uma opção inválida! Tente novamente")
+
+            # Guarda a orientação
+            while True:
+                opcoes = ['H', 'V']
+                orientacao = str(input("Informe a orientação [ v | h ]: ")).upper()
+                if orientacao in opcoes:
+                    break
+                else:
+                    print(f"'{orientacao}' é uma opção inválida! Tente novamente")
+
+            try:
+                mapa_j = aloca_navio(mapa_j, letra+linha, blocos, orientacao)
+                print_mapa(mapa_c, mapa_j)
+                break
+            except Exception:
+                pass
+
+    # Da início ao jogo, loop com contador
+    print("\nIniciando a batalha naval!")
+    for i in reversed(range(6)):
+        time.sleep(1)
+        print(f"{i}")
+
+    # Escolhe, aleatoriamente, as coordenadas do tiro do computador
+    tiros_possiveis = []
+    for i in range(len(COLUNAS)):
+        for j in range(len(LINHAS)):
+            tiros_possiveis.append(f"{COLUNAS[i]}{LINHAS[j]}")
+
+    while True:
+        #Printa os mapas, do computador visível para o jogador, e do jogador
+        print_mapa(mapa_c, mapa_j)
+        
+        #pergunta-se quais as coordenadas do tiro do jogador
+        print("\nDefine as coordenadas do seu próximo disparo")
